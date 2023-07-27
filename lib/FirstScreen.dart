bc5a50509b78f964745715b1bcfae871fd9c10d0
midohamza22 <91686000+midoHaz@users.noreply.github.com>
2023-07-27 10:22:42 +0300

    remove comments

---
+++ lib/FirstScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(onPressed: () {}, icon: const Icon(Icons.person)),
+          title: const Text("Person App"),
+        ),
+      ),
+    );
+  }
+}
